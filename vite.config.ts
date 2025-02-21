05f53a25f44c014bc20df598953ea69c32d16303
Connell Hecht <checht@unknownca81300d49ca.attlocal.net>
2025-02-21 17:11:17 -0600

    Use docs folder for publishing for convenience

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',       // Output to dist for production
+    outDir: 'docs',       // Output to docs for production
     emptyOutDir: true,
   },
   server: {
